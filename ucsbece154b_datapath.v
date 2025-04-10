8f0da44b15d0fc064269ef0c49d96d719d511cac
diegom44 <148291453+diegom44@users.noreply.github.com>
2025-04-10 11:34:22 -0700

    Update ucsbece154b_datapath.v

--- ucsbece154b_datapath.v
+++ ucsbece154b_datapath.v
@@ -39,54 +39,37 @@ module ucsbece154b_datapath (
     output reg     [4:0] RdW_o,
     input          [1:0] ResultSrcM_i
 );
-ß
+
 `include "ucsbece154b_defines.vh"
 
-// Fetch and Decode Stage Registers 
-reg [31:0] PCD; // PC in Decode 
-reg [31:0] InstrD; // Instruction in Decode 
+// Pipeline Registers
+// fetch to decode 
+reg [31:0] PCF, InstrD;
 
-reg [31:0] PCPlus4E, PCPlus4M, PCPlus4W; // PC + 4 in Execute, Memory, Writeback
-
-
-
-wire [31:0] imm_J;
-wire [31:0] target_address = PCD + imm_J; // PC + JAL immediate
-assign imm_J = {12{InstrD[31]}, InstrD[19:12], InstrD[20], InstrD[30:21], 1'b0}; // JAL immediate
+// decode to execute
+reg [31:0] Rd1E, Rd2E, PCE, ImmExtE, PCPlus4E, PCTargetE;
+reg [4:0] Rs1E, Rs2E, RdE;
 
 
-
-always @(posedge clk) begin
-    if (reset)
-        PCF_o <= pc_start // reset PC to start address
-    else if (PCSrcE_i)
-        PCF_o <= target_address; // set PC to target address for JAL instruction
-    else
-        PCF_o <= PCF_o + 4; // increment PC nomrally 
-end
-
-assign op_o InstrD[6:0]; // opcode from instruction
-
-always @(posedge clk) begin
-    PCPlus4E <= PCD + 4; // PC + 4 in Execute stage
-    PCPlus4M <= PCPlus4E; // PC + 4 in Memory stage
-    PCPlus4W <= PCPlus4M; // PC + 4 in Writeback stage
-end
+// execute to memory
 
 
-// Mux to write PC + 4 to the destination register 
-wire [31:0] ResultW = (ResultSrcW_i == MuxResult_PCPlus4) ? PCPlus4W : 32'b0; // Result for Writeback stage
+// memory to writeback
 
-// update destination register 
-always @(posedge clk) begin
-    if (RegWriteW_i)
-    RdW_o <= InstrD[11:7]; // write register in Writeback stage
-end 
+// Instruction Fetch Stage
 
-always @(posedge clk) begin
-    PCD <= PCF_o; // update PC in Decode stage
-    InstrD <= InstrF_i; // update instruction in Decode stage
-end 
+wire [31:0] PCNextF;
+
+assign PCPlus4 = PCF_o + 32'b100; //current PC + 4
+assign PCTarget = PCF_o + ImmExtE; // uses the current PC + Immediate to get to the target address / instruction
+assign PCNextF = (PCSrcE_i) ? PCTargetE : PCPlus4; // mux that either transfers the PC
+
+
+
+
+
+
+
 
 endmodule
 
