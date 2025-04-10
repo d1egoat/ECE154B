0f9c0095431bd222d322afe052383d99dd023499
diegom44 <148291453+diegom44@users.noreply.github.com>
2025-04-10 11:33:41 -0700

    Update ucsbece154b_controller.v

--- ucsbece154b_controller.v
+++ ucsbece154b_controller.v
@@ -39,38 +39,115 @@ module ucsbece154b_controller (
 
  `include "ucsbece154b_defines.vh"
 
- always @ * begin
-    // default values for I-type instructions
-    ImmSrcD_o = imm_Itype; // I-type immediate
-    PCSrcE_o = 1'b0; // default to PCSrc = 0 since we are not jumping or branching
-    ALUSrcE_o = 1'b1; // default to ALUSrc = 1 since we are using I-type instructions
-    MemWriteM_o = 1'b0; // default to MemWrite = 0 for I-type since we are not writing to memory
-    ALUControlE_o = AluControl_add; // default to addition for I-type instructions
-    ResultSrcW_o = MuxResult_aluout; // default to ALU result for I-type instructions
-    branch = 1'b0; // default to branch = 0 for I-type instructions
-    jump = 1'b0; // default to jump = 0 for I-type instructions
-    ALUOp = 2'b00; // default to ALUOp = 0 for I-type instructions
-    RegWreiteW_o = 1'b1; // default to write
 
+
+ wire branch;
+ wire jump;
+ wire [1:0] ALUOp;
+
+
+reg [11:0] controls;
+
+assign PCSrcE_o = (branch & ZeroE_i) | jump; // PCSrc is set to 1 if branch or jump is taken
+assign ImmSrcD_o = controls[10:8]; 
+assign branch = controls[3]; 
+assign ALUOp = controls[2:1]; 
+assign jump = controls[0]; 
+
+always @* begin
     case (op_i)
-        instr_jal_op: begin // JAL instruction
-            ImmSrcD_o = imm_Jtype; // JAL immediate
-            AluSrc_o = 1'bx // set ALUSrc to x for JAL instruction
-            branch = 1'b0; // no branch for JAL instruction
-            jump = 1'b1; // set jump to 1 for JAL instruction
-            PCSrcE_o = PCTarget; // set PCSrc to target address for JAL instruction
-            AluOp = ALUop_dc // set ALUOp to x for JAL instruction   
-            FlushD_o = 1'b1; // flush the instruction in the decode stage
-            FlushE_o = 1'b1; // flush the instruction in the execute stage
-            MemWrite_o = 1'b0; // no memory write for JAL instruction
-            ResultSrc_o = MuxPC_PCPlus4; // set ResultSrc to PC + 4 for JAL instruction
-            RegWriteW_o = 1'b1; // set RegWrite to 1 for JAL instruction
-        end
+        instr_jal_op:       controls = 12'b1_011_x_0_10_0_xx_1;
+        instr_lui_op:       controls = 12'b1_100_1_0_11_0_xx_0;
+        instr_lw_op:        controls = 12'b1_000_1_0_01_0_00_0;       
+	    instr_sw_op:        controls = 12'b0_001_1_1_xx_0_00_0;  
+	    instr_Rtype_op:     controls = 12'b1_xxx_0_0_00_0_10_0;   
+	    instr_beq_op:       controls = 12'b0_010_0_0_xx_1_01_0;   
+	    instr_ItypeALU_op:  controls = 12'b1_000_1_0_00_0_10_0;
+        default: begin	    
+                            controls = 12'bx_xxx_x_x_xx_x_xx_x;       
+            `ifdef SIM
+                $warning("Unsupported op given: %h", op_i);
+            `else
+            ;
+            `endif
+            
+        end 
     endcase
- end 
-    
-    
+
+    RegWriteW_o = controls[11]; // register write
+    ALUSrcE_o = controls[7]; // ALU source
+    MemWriteM_o = controls[6]; // memory write
+    ResultSrcW_o = controls[5:4]; // result source
+end
+
+// Add this after main decoder
+always @* begin
+    case(ALUOp)
+        ALUop_mem: ALUControlE_o = ALUcontrol_add;  // LW/SW
+        ALUop_beq: ALUControlE_o = ALUcontrol_sub;  // BEQ
+        ALUop_other: begin  // R-type/I-type
+            case(funct3_i)
+                3'b000: ALUControlE_o = funct7b5_i ? 
+                                      ALUcontrol_sub : ALUcontrol_add;
+                3'b010: ALUControlE_o = ALUcontrol_slt;
+                3'b110: ALUControlE_o = ALUcontrol_or;
+                3'b111: ALUControlE_o = ALUcontrol_and;
+                default: ALUControlE_o = 3'bxxx;
+            endcase
+        end
+        default: ALUControlE_o = 3'bxxx;
+    endcase
+end
 
 
+wire RtypeSub;
+assign RtypeSub = funct7b5_i & op_i[5]; // R-type subtract instruction
+
+always @* begin
+    case (ALUOp)
+        ALUop_mem: ALUControlE_o = ALUcontrol_add;  // LW/SW
+        ALUop_beq: ALUControlE_o = ALUcontrol_sub;  // BEQ
+        ALUop_other:   // R-type/I-type
+            case(funct3_i)
+                instr_addsub_funct3:
+                    if(RtypeSub)   ALUControlE_o = ALUcontrol_sub; // subtraction else 
+                    else           ALUControlE_o = ALUcontrol_add; // addition
+                instr_slt_funct3:  ALUControlE_o = ALUcontrol_slt;  // set less than (101)
+                instr_or_funct3:   ALUControlE_o = ALUcontrol_or; // or operation (011)
+                instr_and_funct3:  ALUControlE_o = ALUcontrol_and;  // and operation (010)
+            default: begin
+                            ALUControlE_o = 3'bxxx;
+                `ifdef SIM
+                    $warning("Unsupported funct3 given: %h", funct3_i);
+                `else
+                ;
+                `endif  
+            end
+            endcase
+    default: 
+        `ifdef SIM
+            $warning("Unsupported ALUop given: %h", ALUOp);
+        `else
+        ;
+        `endif   
+    endcase
+end
+
+assign FlushD_o = jump;  // Flush decode stage on jumps
+assign FlushE_o = jump;  // Flush execute stage on jumps
+
+// Stall signals (simplified example)
+assign StallF_o = 1'b0;  // No stalling in this example
+assign StallD_o = 1'b0;  // No stalling in this example
+
+// Simple forwarding example
+always @* begin
+    ForwardAE_o = (Rs1E_i == RdM_i) ? 2'b10 : 
+                 (Rs1E_i == RdW_i) ? 2'b01 : 
+                 2'b00;
+    ForwardBE_o = (Rs2E_i == RdM_i) ? 2'b10 : 
+                 (Rs2E_i == RdW_i) ? 2'b01 : 
+                 2'b00;
+end
 endmodule
 
