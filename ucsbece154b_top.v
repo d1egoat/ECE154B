06746d5582dc3c6498a608cf3c339f4b3d736335
Diego Mateos <148291453+d1egoat@users.noreply.github.com>
2025-04-13 23:42:15 -0700

    Update ucsbece154b_top.v

--- ucsbece154b_top.v
+++ ucsbece154b_top.v
@@ -7,11 +7,15 @@
 
 module ucsbece154b_top (
     input clk, reset
+    output [31:0] InstD,
+    output [31:0] RD1E, RD2E, ExtImmE,
+    output [31:0] ResultW
 );
 
 wire [31:0] pc, instr, readdata;
 wire [31:0] writedata, dataadr;
 wire        memwrite;
+
 
 // processor and memories are instantiated here
 ucsbece154b_riscv_pipe riscv (
@@ -22,6 +26,11 @@ ucsbece154b_riscv_pipe riscv (
     .ALUResultM_o(dataadr), 
     .WriteDataM_o(writedata),
     .ReadDataM_i(readdata)
+    .InstD_o(InstD),
+    .RD1E_o(RD1E),
+    .RD2E_o(RD2E),
+    .ImmExtE_o(ImmExtE),
+    .ResultW_o(ResultW)
 );
 ucsbece154_imem imem (
     .a_i(pc), .rd_o(instr)
